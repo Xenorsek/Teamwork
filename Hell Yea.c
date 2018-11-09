02937638842067ee6cedb4885c2ebcaf0a623977
BiM4Bush <44493022+BiM4Bush@users.noreply.github.com>
2018-11-09 10:03:40 +0100

    Mnozenie i dzielenie
    dwa ode mnie

--- Hell Yea.c
+++ Hell Yea.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<math.h>
 int main()
 {
   printf("HELLO WORLD");
@@ -7,34 +6,29 @@ int main()
   int liczba1,liczba2;
   char znak;
   
-  printf("xd");
   
-  /*switch(x)
-    case 1:
-            '+': x=a+b; 
-                  printf("Wynik: %d\n", x);
-             break;
+  switch(x)
+  {
+     case 1:
   
-  case 2:
-            '-': x=a-b;
-                  printf("Wynik: %d\n", x);
-             break;
+     case 2:
   
-  case 3:
+     case 3:
+         printf("Wybrano mnożenie, podaj dwie liczby:  ,  ",liczba1,liczba2);
+         scanf("%i",&liczba1);
+         scanf("%i"&liczba2);
+         printf("Wynik= %d*%d",liczba1,liczba2);
+         break;      
+     case 4:
+         printf("Wybrano dzielenie, podaj dwie liczby:  ,  ",liczba1,liczba2);
+         scanf("%i",&liczba1);
+         scanf("%i"&liczba2);
+         printf("Wynik = %d/%d",liczba1,liczba2);
+         break;  
+     case 5:
   
-  case 4:
+     case 6:
+  }
   
-  case 5:
-      printf("Wybrales potegowanie, wprowadz dwie liczby\n");
-      scanf("%i %i",liczba1, liczba2);
-      printf(liczba1^liczba2);
-  break;
-  case 6:
-    printf("Wybrales pierwiastkowanie, wprowadz dwie liczby\n");
-    scanf("%i %i",liczba1, liczba2);
-    liczba2=liczba2^(-1);
-    printf(liczba1^liczba2);
- break;
-  */
   return 0;
 }
